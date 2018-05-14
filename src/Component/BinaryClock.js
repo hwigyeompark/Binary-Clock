03757bb15ddc76a4deb7fd48a7618faa370f5290
hwigyeompark <phg5590@gmail.com>
2018-05-14 23:04:06 +0900

    update BinaryClock and Number

--- src/Component/BinaryClock.js
+++ src/Component/BinaryClock.js
@@ -1,4 +1,5 @@
 import React, {Component} from 'react';
+import Number from "./Number";
 
 class BinaryClock extends Component{
     state = {
@@ -16,12 +17,19 @@ class BinaryClock extends Component{
       const mm = now.getMinutes().toString().padStart(2,'0');
       const ss = now.getSeconds().toString().padStart(2,'0');
       this.setState({
-          hour1 : hh.substr(0,1)
-      })
+          hour1 : hh.substr(0,1),
+          hour2 : hh.substr(-1),
+          minute1 : mm.substr(0,1),
+          minute2 : mm.substr(-1),
+          second1 : ss.substr(0,1),
+          second2 : ss.substr(-1)
+      });
     };
     render(){
         return(
-            <div></div>
+            <div>
+                <Number/>
+            </div>
         );
     }
 }
