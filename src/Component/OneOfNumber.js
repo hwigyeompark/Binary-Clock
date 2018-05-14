b7a9a896f3e3fe4e7ae8dbd66bdfc0da5980c912
hwigyeompark <phg5590@gmail.com>
2018-05-14 23:19:45 +0900

    update OneOfNumber

--- src/Component/OneOfNumber.js
+++ src/Component/OneOfNumber.js
@@ -1,6 +1,18 @@
 import React, {Component} from 'react';
 
 class OneOfNumber extends Component{
+    state = {
+        shape : '○'
+    };
+
+    static getDerivedStateFromProps(nextProps, prevState){
+        console.log('getDerivedStateFromProps');
+        console.log(nextProps);
+        console.log(prevState);
+        return{
+            shape:nextProps.display === '0' ? '○':'●'
+        }
+    }
     render(){
         return(
           <div></div>
